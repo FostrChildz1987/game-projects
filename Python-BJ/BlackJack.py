c75a2c3a66480f3c6034b90eaa8ae1d78d10d01b
FostrChildz1987 <gmodafton@gmail.com>
2023-02-02 19:47:41 -0500

    added more stuff

--- Python-BJ/BlackJack.py
+++ Python-BJ/BlackJack.py
@@ -40,9 +40,57 @@ class Card:
             cardName = str(self.num) + self.cardType
         return cardName
 
+def betLoop(payroll):
+    global bet
+    betLoop = True
+    while betLoop:
+        try:
+            print("You have:", payroll, "chips")
+            bet = float(input("Enter your bet amount: "))
+        except:
+            print("Please enter a valid number!")
+        else:
+            if(bet <= payroll):
+                betLoop = False
+            else:
+                print("You must enter a valid bet!")
+
+def choiceLoop():
+    global choice
+    choiceLoop = True
+    while choiceLoop:
+        choice = input("Would you like to (h)it or (s)tand?: ")
+        if(choice == "h"):
+            choiceLoop = False
+        elif(choice == "s"):
+            choiceLoop = False
+        else:
+            print("Enter a valid value!")
+
+def playerDraw():
+    pass
+
+def dealerTurn():
+    pass
+
+def dealerDraw():
+    pass
+
+def win():
+    pass
+
+def loss():
+    pass
+
+
+
+
+
+
 
 def initializeGame():
     global diff
+    global payroll
 
     print("Welcome to Bigus Dickus Blackjack!")
     print()
@@ -52,6 +100,7 @@ def initializeGame():
     print("Royal cards (King, Queen, Jack) are worth 10 points!")
     print("Aces are worth 1 or 11 points!")
     print("All other cards are worth face value!")
+    print("Earn 100,000 Chips to win!")
     print()
     
     loop = True
@@ -63,5 +112,34 @@ def initializeGame():
         else:
             print("Please enter a valid input!")
             print()
+    
+    if(diff == "e"):
+        payroll = 20000.0
+    elif(diff == "m"):
+        payroll = 10000.0
+    elif(diff == "h"):
+        payroll = 5000.0
 
-def mainGame(difficulty)
+def mainGame(difficulty):
+    betLoop(payroll)
+    print("You have bet:", bet, "chips!\n")
+
+    d1 = Card()
+    d2 = Card()
+    p1 = Card()
+    p2 = Card()
+    pList = [p1.num, p2.num]
+    dList = [d1.num, d2.num]
+
+    print("The dealer's face card is:", d1.name())
+
+    print("Your cards are:", p1.name(), "and", p2.name(), "Total:", str(sum(pList)))
+    print()
+    
+    choiceLoop()
+    if(choice == "h"):
+        playerDraw()
+    elif(choice == "s"):
+        dealerTurn()
+
+    
